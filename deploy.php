b01c059ba5b7b315d920d4c83723262854bf521f
henki wisnu subakti <henki.subakti@gmail.com>
2024-09-09 15:40:21 +0700

    update

---
+++ deploy.php
@@ -0,0 +1,74 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/npm.php';
+
+set('bin/php', function () {
+    return '/usr/local/bin/php'; // change
+});
+
+set('application', 'CICD'); 
+set('repository', 'git@github.com:henkiws/CICD.git');
+
+set('git_tty', true);
+set('git_ssh_command', 'ssh -o StrictHostKeyChecking=no');
+
+set('keep_releases', 5);
+
+set('writable_mode', 'chmod'); // shared hosting
+
+// Shared files/dirs between deploys
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
+
+// Writable dirs by web server
+add('writable_dirs', [
+    "bootstrap/cache",
+    "storage",
+    "storage/app",
+    "storage/framework",
+    "storage/logs",
+]);
+
+set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
+
+// Hosts
+host('NAMA_REMOTE_HOST') // Nama remote host server ssh anda | contoh host('NAMA_REMOTE_HOST')
+->setHostname('139.59.225.77') // Hostname atau IP address server anda | contoh  ->setHostname('10.10.10.1') 
+->set('remote_user', 'henki') // SSH user server anda | contoh ->set('remote_user', 'u1234567')
+->set('port', 22) // SSH port server anda, untuk kasus ini server yang saya gunakan menggunakan port custom | contoh ->set('remote_user', 65002)
+->set('branch', 'master') // Git branch anda
+->set('deploy_path', '~/var/www/dev.hafidan.my.id'); // Lokasi untuk menyimpan projek laravel pada server | contoh ->set('deploy_path', '~/public_html/api-deploy');
+
+
+
+// Tasks
+
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+desc('Build assets');
+task('deploy:build', [
+    'npm:install',
+]);
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:secrets',       // Deploy secrets
+    'deploy:vendors',
+    'deploy:shared',
+    'artisan:storage:link',
+    'artisan:queue:restart',
+    'deploy:publish',
+    'deploy:unlock',
+]);
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release. Uncomment below code if you want to migrate after deploy
+
+// before('deploy:symlink', 'artisan:migrate');
